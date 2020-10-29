eaa4c054d76db5cbc0e5e3a6fe59ebce8ec001a9
Pavel Zwerschke <pavelzw@gmail.com>
2020-10-29 04:11:35 +0100

    Add image conversion

--- __init__.py
+++ __init__.py
@@ -23,10 +23,31 @@ class KindleComics(InputFormatPlugin):
                              recommended_value='auto', help='What color should the margins have.'),
         OptionRecommendation(name='no_greyscale', recommended_value=False,
                              help='Don\'t convert the image to grayscale (black and white).'),
-        OptionRecommendation(name='max_width', recommended_value=1264,
+        OptionRecommendation(name='max_width', recommended_value="1264",
                              help='Maximum width.'),
-        OptionRecommendation(name='max_height', recommended_value=1680,
+        OptionRecommendation(name='max_height', recommended_value="1680",
                              help='Maximum height.'),
+
+        OptionRecommendation(name='bordersColor', recommended_value=True,
+                             help='PLACEHOLDER.'),
+        OptionRecommendation(name='profileData', recommended_value=0, #todo gamma
+                             help='PLACEHOLDER.'),
+        OptionRecommendation(name='hq', recommended_value=True,
+                             help='PLACEHOLDER.'),
+        OptionRecommendation(name='forceColor', recommended_value=False,
+                             help='PLACEHOLDER.'),
+        OptionRecommendation(name='forcepng', recommended_value=False,
+                             help='PLACEHOLDER.'),
+        OptionRecommendation(name='gamma', recommended_value=0,
+                             help='PLACEHOLDER.'),
+        OptionRecommendation(name='stretch', recommended_value=False,
+                             help='PLACEHOLDER.'),
+        OptionRecommendation(name='kfx', recommended_value=False,
+                             help='PLACEHOLDER.'),
+        OptionRecommendation(name='upscale', recommended_value=True,
+                             help='PLACEHOLDER.'),
+        OptionRecommendation(name='format', recommended_value="CBR",
+                             help='PLACEHOLDER.'),
     }
 
     def gui_configuration_widget(self, parent, get_option_by_name, get_option_help, db, book_id=None):
@@ -37,12 +58,65 @@ class KindleComics(InputFormatPlugin):
         from calibre_plugins.kindle_comics.make_book import make_book
         book = os.path.abspath(stream.name)
         stream.close()
-        print("STREAM: ", stream)
-        print("OPTIONS: ", options)
-        print("FILE_EXT: ", file_ext)
-        print("LOG: ", log)
-        print("ACCELERATORS: ", accelerators)
-        make_book(book, options, log)
+        make_book(_convert_options_to_dict(options), book, log)
+
         print("SLEEPING " + options.max_width)
-        time.sleep(int(options.max_width))
+        if options.manga:
+            time.sleep(30)
         return None
+
+
+def _convert_options_to_dict(options):
+    # self.options is only usable in __init__.py, it needs to be converted for other modules
+    palette = [
+        0x00, 0x00, 0x00,
+        0x11, 0x11, 0x11,
+        0x22, 0x22, 0x22,
+        0x33, 0x33, 0x33,
+        0x44, 0x44, 0x44,
+        0x55, 0x55, 0x55,
+        0x66, 0x66, 0x66,
+        0x77, 0x77, 0x77,
+        0x88, 0x88, 0x88,
+        0x99, 0x99, 0x99,
+        0xaa, 0xaa, 0xaa,
+        0xbb, 0xbb, 0xbb,
+        0xcc, 0xcc, 0xcc,
+        0xdd, 0xdd, 0xdd,
+        0xee, 0xee, 0xee,
+        0xff, 0xff, 0xff,
+    ]
+    opts = {
+        'manga': options.manga,
+        'webtoon': options.webtoon,
+        'margins': options.margins,
+        'no_greyscale': options.no_greyscale,
+        'max_width': int(options.max_width),
+        'max_height': int(options.max_height),
+
+        'bordersColor': None,
+        'profileData': ['Kindle Paperwhite 3/4/Voyage/Oasis', (1072, 1448), palette, 1.8],
+        'hq': False,
+        'forceColor': False,
+        'forcecolor': False,
+        'forcepng': False,
+        'gamma': 0,
+        'stretch': False,
+        'kfx': False,
+        'upscale': True,
+        'format': "MOBI",
+        'cropping': 2,
+        'croppingp': 1.0
+
+        # 'bordersColor': options.bordersColor,
+        # 'profileData': options.profileData,
+        # 'hq': options.hq,
+        # 'forceColor': options.forcecolor,
+        # 'forcepng': options.forcepng,
+        # 'gamma': options.gamma,
+        # 'stretch': options.stretch,
+        # 'kfx': options.kfx,
+        # 'upscale': options.upscale,
+        # 'format': options.format,
+    }
+    return opts
--- make_book.py
+++ make_book.py
@@ -1,24 +1,31 @@
 import os
+import random
+import sys
+from multiprocessing import Pool
 from shutil import copytree, move, rmtree
 
+from calibre.ebooks.metadata import MetaInformation
+from calibre.gui2 import Dispatcher
 from calibre.ptempfile import PersistentTemporaryDirectory
 from calibre.utils.logging import INFO
-from calibre_plugins.kindle_comics.corruption_detector import detect_corruption
-global log
+global _log, _options
 
 
-def make_book(comic_file, options, _log):
-    global log
-    log = _log
+def make_book(options, comic_file, log):
+    global _log, _options
+    _options = options
+    print(options)
+    _log = log
     log.prints(INFO, "Extracting images.")
     path = get_work_folder(comic_file)
-    log.prints(INFO, "Checking images.")
+    log.prints(INFO, "Processing images.")
+    img_directory_processing(path)
 
 
 def get_work_folder(comic_file):
     from calibre.ebooks.comic.input import extract_comic
     target_dir = extract_comic(comic_file)
-    log.prints(INFO, "Extracted images into " + target_dir)
+    _log.prints(INFO, "Extracted images into " + target_dir)
 
     # move file structure up
     subdirectories = os.listdir(target_dir)
@@ -32,6 +39,53 @@ def get_work_folder(comic_file):
     new_path = PersistentTemporaryDirectory(suffix='_oebps')
     copytree(target_dir, os.path.join(new_path, 'OEBPS', 'Images'))
     rmtree(target_dir)
-    log.prints(INFO, "Moved images to structure in " + new_path)
+    _log.prints(INFO, "Moved images to structure in " + new_path)
+
     return new_path
 
+
+def img_directory_processing(path):
+    img_metadata = {}
+    img_old = []
+    work = []
+    page_number = 0
+    for dir_path, _, filenames in os.walk(path):
+        for file in filenames:
+            page_number += 1
+            work.append([file, dir_path])
+    if len(work) > 0:
+        for i in work:
+            _log.prints(INFO, "Processing file", i[0])
+            output = img_file_processing(i[0], i[1])
+
+            for page in output:
+                # additional metadata for the page (rotated, margins)
+                img_metadata[page[0]] = page[1]
+                # remove old images
+                if page[2] not in img_old:
+                    img_old.append(page[2])
+        for file in img_old:
+            os.remove(file)
+    else:
+        rmtree(os.path.join(path, '..', '..'), True)
+        raise UserWarning("Source directory is empty.")
+
+
+def img_file_processing(file, dir_path):
+    from calibre_plugins.kindle_comics.image import ComicPageParser, ComicPage
+    output = []
+    work_img = ComicPageParser((dir_path, file), _options)
+    for i in work_img.payload:
+        img = ComicPage(_options, *i)
+        if _options['cropping'] == 2 and not _options['webtoon']:
+            img.cropPageNumber(_options['croppingp'])
+        if _options['cropping'] > 0 and not _options['webtoon']:
+            img.cropMargin(_options['croppingp'])
+        print("Autocontrast")
+        img.autocontrastImage()
+        print("Resize")
+        img.resizeImage()
+        if _options['forcepng'] and not _options['forcecolor']:
+            img.quantizeImage()
+        output.append(img.saveToDir())
+    return output
